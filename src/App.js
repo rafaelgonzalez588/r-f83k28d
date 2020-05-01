19bac363c0f82d75b48a29ff0f333a983047899a
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-04-30 20:00:03 -0500

    agregando contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    contador: 0,
+  };
+  
+  cuenta = () => {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  };
+  
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.contador}</span>
+        <button onClick={this.cuenta} id="inc">Incrementa</button>
       </div>
     );
   }
